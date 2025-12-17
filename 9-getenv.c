87dac21a9b2cda0845d2c4639358078df3a176b8
Bengin05 <uzunbengin9@gmail.com>
2025-12-17 15:15:22 +0000

    exerxice 9

---
+++ 9-getenv.c
@@ -0,0 +1,24 @@
+#include <string.h>
+
+extern char **environ;
+
+char *_getenv(const char *name)
+{
+    int i = 0;
+    size_t len;
+
+    if (name == NULL)
+        return (NULL);
+
+    len = strlen(name);
+
+    while (environ[i] != NULL)
+    {
+        if (strncmp(environ[i], name, len) == 0 && environ[i][len] == '=')
+        {
+            retunr (environ[i] + len + 1);
+        }
+        i++;
+    }
+    return (NULL);
+}
