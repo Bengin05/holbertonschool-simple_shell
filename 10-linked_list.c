13c6b2bc26ac2a66f77ddde418d3123b8054305c
Bengin05 <uzunbengin@gmail.com>
2025-12-19 09:02:05 +0000

    exercice 10

--- 10-linked_list.c
+++ 10-linked_list.c
@@ -5,47 +5,75 @@ extern char **environ;
 
 typedef struct s_path
 {
-    char    *dir;
-    struct s_path   *next;
+	char *dir;
+	struct s_path *next;
 } t_path;
 
 t_path *linked_list(const char *name)
 {
-    int i = 0;
-    size_t len;
-    char *paty_copy;
-    char *start;
-    char *end;
-    t_path *head = NULL;
-    t_path *current = NULL;
+	int i = 0;
+	size_t len;
+	char *path_copy;
+	char *start;
+	char *end;
+	t_path *head = NULL;
+	t_path *current = NULL;
+	t_path *node;
 
-    if (name == NULL)
-        return (NULL);
+	if (name == NULL)
+		return (NULL);
 
-    len = strlen(name);
+	len = strlen(name);
 
-        while (environ[i] != NULL)
-    {
-        if (strncmp(environ[i], name, len) == 0 && environ[i][len] == '=')
-        {
-            paty_copy = strdup(environ[i] + len + 1);
-            if (!paty_copy)
-                return (NULL);
-            break;
-        }
-        i++;
-    }
-    if (environ[i] == NULL)
-        return NULL;
+	while (environ[i] != NULL)
+	{
+		if (strncmp(environ[i], name, len) == 0 &&
+		    environ[i][len] == '=')
+		{
+			path_copy = strdup(environ[i] + len + 1);
+			if (path_copy == NULL)
+				return (NULL);
+			break;
+		}
+		i++;
+	}
+	if (environ[i] == NULL)
+		return (NULL);
 
-    start = paty_copy;
-    while (*start)
-    {
-        end = start;
-        while (*end && *end != ':')
-            end++;
-        
-    }
+	start = path_copy;
+	while (*start)
+	{
+		end = start;
+		while (*end && *end != ':')
+			end++;
 
-    char *paty_copy = strdup(environ[i] + len + 1);
+		node = malloc(sizeof(t_path));
+		if (node == NULL)
+		{
+			free(path_copy);
+			return (NULL);
+		}
+		node->dir = malloc((end - start) + 1);
+		if (node->dir == NULL)
+		{
+			free(node);
+			free(path_copy);
+			return (NULL);
+		}
+		strncpy(node->dir, start, end - start);
+		node->dir[end - start] = '\0';
+		node->next = NULL;
+
+		if (head == NULL)
+			head = node;
+		else
+			current->next = node;
+		current = node;
+		if (*end == ':')
+			start = end + 1;
+		else
+			start = end;
+	}
+	free(path_copy);
+	return (head);
 }
